2f4423593c94317815d63845908def23c2823a50
andrewchantzos <andrewhantzos@gmail.com>
2021-04-06 19:20:21 +0100

    Initial check-in of module AndreasTest

---
+++ BankTests/UnitTest1.cs
@@ -0,0 +1,31 @@
+using NUnit.Framework;
+
+using BankAccountNS;
+
+namespace BankTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Debit_WithValidAmount_UpdatesBalance()
+        {
+            // Arrange
+            double beginningBalance = 11.99;
+            double debitAmount = 4.55;
+            double expected = 7.44;
+            BankAccount account = new BankAccount("Mr. Bryan Walton", beginningBalance);
+
+            // Act
+            account.Debit(debitAmount);
+
+            // Assert
+            double actual = account.Balance;
+            Assert.AreEqual(expected, actual, 0.001, "Account not debited correctly");
+        }
+    }
+}
